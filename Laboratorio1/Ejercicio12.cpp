2ab034512a61262dfcc0cd6f557bccb674ea5e67
EduardoDominguez1 <00136319@uca.edu.sv>
2019-09-02 20:35:57 -0600

    Arreglando ejercicio12

--- Laboratorio1/Ejercicio12.cpp
+++ Laboratorio1/Ejercicio12.cpp
@@ -3,20 +3,27 @@
 
 using namespace std;
 
+int InveritrCifra(int a);
+
 int main(){
 
 int Cifra = 0;
-int CifraInv = 0;
+int CifraInvertida  = 0;
 
-cout << "Ingrese cifra que desea invertir: " << endl;
+	cout << "Ingrese cifra que desea invertir: " << endl;
     cin >> Cifra;
-
-
-while(Cifra > 0){
-    CifraInv = CifraInv*10 + Cifra%10;
-    Cifra = Cifra/10;
-}
- cout << "La cifra inversa es: " << CifraInv << endl;
-
+	
+	CifraInvertida = InveritrCifra(Cifra);
+	cout << "La cifra inversa es: " << CifraInvertida << endl;
     return 0;
+}
+
+int InveritrCifra(int a){
+
+int CifraInv = 0;
+while(a > 0){
+    CifraInv = CifraInv*10 + a%10;
+    a = a/10;
+}
+return CifraInv;
 }
